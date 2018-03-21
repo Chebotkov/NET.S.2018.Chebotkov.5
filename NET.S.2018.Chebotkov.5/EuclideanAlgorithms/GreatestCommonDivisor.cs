c12219eaa4791921e2e49468ca132211708e5c7b
Pavel <chpv24fev@gmail.com>
2018-03-21 23:48:52 +0300

    euclidean

---
+++ NET.S.2018.Chebotkov.5/EuclideanAlgorithms/GreatestCommonDivisor.cs
@@ -0,0 +1,85 @@
+﻿namespace EuclideanAlgorithms
+{
+    /// <summary>
+    /// This class contains euclidean algorithm implementation. 
+    /// </summary>
+    public class GreatestCommonDivisor
+    {
+        /// <summary>
+        /// This method implements euclidean algorithm.
+        /// </summary>
+        /// <param name="firstNumber">First number.</param>
+        /// <param name="secondNumber">Second number.</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetGCD(int firstNumber, int secondNumber)
+        {
+            if(firstNumber == 0 || secondNumber == 0)
+            {
+                throw new System.ArgumentOutOfRangeException("Number can't be equal 0");
+            }
+
+            if(firstNumber<0)
+            {
+                firstNumber = -firstNumber;
+            }
+
+            if(secondNumber<0)
+            {
+                secondNumber = -secondNumber;
+            }
+
+            while (firstNumber != 0 && secondNumber != 0)
+            {
+                if (firstNumber % secondNumber == 0)
+                {
+                    return secondNumber;
+                }
+
+                if (secondNumber % firstNumber == 0)
+                {
+                    return firstNumber;
+                }
+
+                if (secondNumber > firstNumber)
+                {
+                    secondNumber %= firstNumber;
+                }
+
+                if (firstNumber > secondNumber)
+                {
+                    firstNumber %= secondNumber;
+                }
+            }
+
+            return firstNumber + secondNumber;
+        }
+
+        /// <summary>
+        /// This method implements euclidean algorithm.
+        /// </summary>
+        /// <param name="firstNumber">First number.</param>
+        /// <param name="secondNumber">Second number.</param>
+        /// <param name="thirdNumber">Third number.</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetGCD(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return GetGCD(GetGCD(firstNumber, secondNumber), thirdNumber);
+        }
+
+        /// <summary>
+        /// This method implements euclidean algorithm.
+        /// </summary>
+        /// <param name="numbers">Numbers.</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetGCD(params int[] numbers)
+        {
+            int result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result = GetGCD(result, numbers[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NET.S.2018.Chebotkov.5/GreatestCommonDivisor.Tests/GreatestCommonDivisorTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace GreatestCommonDivisor.Tests
+{
+    /// <summary>
+    /// Test class for GreatestCommonDivisor
+    /// </summary>
+    [TestFixture]
+    public class GreatestCommonDivisorTests
+    {
+        [TestCase(546, 1092, 546, 2184, 4368)]
+        [TestCase(1, 123213, 324234, 24324121, 43683)]
+        [TestCase(3, 171, 1233, 45, 111222111)]
+        public void TestsForGreatestCommonDivisor(int expectedResult, params int[] numbers)
+        {
+            int result = EuclideanAlgorithms.GreatestCommonDivisor.GetGCD(numbers);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(1, 324423, 324233, 232323)]
+        [TestCase(1, 23, 32423343, 325324326)]
+        public void TestsForGreatestCommonDivisor(int expectedResult, int firstNum, int secondNum, int thirdNum)
+        {
+            int result = EuclideanAlgorithms.GreatestCommonDivisor.GetGCD(firstNum, secondNum, thirdNum);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(18, 36, 18)]
+        [TestCase(3, -3, -6)]
+        [TestCase(11, 121, -33)]
+        public void TestsForGreatestCommonDivisor(int expectedResult, int firstNum, int secondNum)
+        {
+            int result = EuclideanAlgorithms.GreatestCommonDivisor.GetGCD(firstNum, secondNum);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(0,0)]
+        [TestCase(0,1)]
+        [TestCase(-1,0)]
+        public void TestsForGreatestCommonDivisor(int firstNum, int secondNum)
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>( () => EuclideanAlgorithms.GreatestCommonDivisor.GetGCD(firstNum, secondNum));
+        }
+
+        [TestCase]
+        public void TestsForGreatestCommonDivisor()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => EuclideanAlgorithms.GreatestCommonDivisor.GetGCD(1, 3, 3, 3, 0, 4));
+        }
+    }
+}
