7fbacf3215e9daed9e1bca0c4a8b91538a1553e6
EPAM RD NET Lab, Minsk <31510338+EPM-RD-NETLAB@users.noreply.github.com>
2018-03-23 11:02:54 +0300

    Added class Notation.

--- NET.S.2018.Chebotkov.5/ConverTo.Tests/ConverToTests.cs
+++ NET.S.2018.Chebotkov.5/ConverTo.Tests/ConverToTests.cs
@@ -8,19 +8,15 @@ namespace ConverTo.Tests
     {
         [TestCase("0110111101100001100001010111111", 2, 934331071)]
         [TestCase("01101111011001100001010111111", 2, 233620159)]
+        [TestCase("1001", 2, 9)]
         [TestCase("1AeF101", 16, 28242177)]
         [TestCase("1ACB67", 16, 1756007)]
         [TestCase("764241", 8, 256161)]
         public void TestsForConverTo(string number, int notation, int expectedResult)
         {
-            uint result = number.ConvertTo(notation);
+            int result = number.ConvertTo(new Notation(notation));
 
-            Assert.AreEqual((uint)expectedResult, result);
-        }
-
-        public void Test()
-        {
-            Assert.AreEqual("11101101111011001100001010111111".ConvertTo(2), 3991716543);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [TestCase("1AeF101", 2)]
@@ -29,13 +25,13 @@ namespace ConverTo.Tests
         [TestCase("1AeF101", 10)]
         public void TestsForGreatestCommonDivisor(string number, int notation)
         {
-            Assert.Throws<System.ArgumentException>(() => number.ConvertTo(notation));
+            Assert.Throws<System.ArgumentException>(() => number.ConvertTo(new Notation(notation)));
         }
 
         [TestCase("111111111111111111111111111111111111111111111111111111", 2)]
         public void TestsForGreatestCommonDivisorOFE(string number, int notation)
         {
-            Assert.Throws<System.OverflowException>(() => number.ConvertTo(notation));
+            Assert.Throws<System.OverflowException>(() => number.ConvertTo(new Notation(notation)));
         }
     }
 }
--- NET.S.2018.Chebotkov.5/ExtensionsForString/Converter.cs
+++ NET.S.2018.Chebotkov.5/ExtensionsForString/Converter.cs
@@ -3,6 +3,45 @@
 namespace ExtensionsForString
 {
     /// <summary>
+    /// This class is about Notation.
+    /// </summary>
+    public class Notation
+    {
+        private int systemBase;
+        private char[] symbols;
+        private int minBoarder = 2, maxBoarder = 16;
+
+        /// <summary>
+        /// Initialization of notation.
+        /// </summary>
+        /// <param name="systemBase">Base of Notation.</param>
+        /// <param name="symbols">Available symbols for notation.</param>
+        public Notation(int systemBase)
+        {
+            if (systemBase < minBoarder || systemBase > maxBoarder)
+            {
+                throw new ArgumentException("Wrong notation. Notation must be: 2<=Notation<=16");
+            }
+
+            this.systemBase = systemBase;
+            GetSymbols();
+        }
+
+        private void GetSymbols()
+        {
+            char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            symbols = new char[systemBase - 1];
+            for (int i = 0; i < systemBase; i++)
+            {
+                symbols[i] = digits[i];
+            }
+        }
+
+        public int SystemBase { get; }
+        public char[] Symbols { get; }
+    }
+
+    /// <summary>
     /// This class contains method which converts string value to integer number with specified notation.  
     /// </summary>
     public static class Converter
@@ -14,17 +53,19 @@ namespace ExtensionsForString
         /// <param name="notation">Notation.</param>
         /// <exception cref="ArgumentException">Throws when wrong number was entered.</exception>
         /// <returns>Returns decimal value of received number.</returns>
-        public static uint ConvertTo(this string number, int notation)
+        public static int ConvertTo(this string number, Notation notation)
         {
-            char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-            uint result = (uint)digits.IndexOf(number[number.Length - 1], notation);
-            uint power = (uint)notation;
+            if (String.IsNullOrEmpty(number))
+            {
+                throw new ArgumentNullException("Number can't be null or empty!");
+            }
+            int result = notation.Symbols.IndexOf(number[number.Length - 1], notation.SystemBase);
+            int power = notation.SystemBase;
             checked
             {
-                for (int i = number.Length - 2; i >= 0; i--, power *= (uint)notation)
+                for (int i = number.Length - 2; i >= 0; i--, power *= notation.SystemBase)
                 {
-                    result += (uint)digits.IndexOf(number[i], notation) * power;
+                    result += notation.Symbols.IndexOf(number[i], notation.SystemBase) * power;
                 }
             }
 
